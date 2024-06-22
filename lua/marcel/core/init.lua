86a7a3f3b57c9000e8fa5ad31e9c55b2a89ff259
Rheilo <marcel.dirkmann@yahoo.de>
2024-06-22 20:12:47 +0200

    Erstellen der Options.lua

---
+++ lua/marcel/core/init.lua
@@ -0,0 +1,1 @@
+require("marcel.core.options")
---
+++ lua/marcel/core/options.lua
@@ -0,0 +1,18 @@
+local options = {
+	number		 			= true,								-- Anzeigen der Zeilennummer
+	relativenumber	= true,								-- Anzeigen der relativen Zeilennummer
+	numberwidth			= 4,									-- Wie viele Ziffern werden von der Zeilennummer angezeigt
+	cursorline	  	= true,
+	clipboard 	  	= "unnamedplus",
+	fileencoding		= "UTF-8",
+  list            = flase,              -- Zeige Steuerzeichen 
+	shiftwidth			= 0,									
+	tabstop					= 2,									-- Gibt an wie viele <space> ein <tab> sind
+	expandtab				= true,								-- Konvertiert <tab> zu <space>
+  smartcase       = true,               -- Ignoriert Groß- & Kleinschreibung in der Suche
+}
+
+for key, value in pairs(options) do
+	vim.opt[key] = value
+end
+
